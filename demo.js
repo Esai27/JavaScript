bc0fb18657a595840c7736a7ec4e8881c5c80768
Esakkiammal <103570885+Esai27@users.noreply.github.com>
2023-09-04 08:44:28 +0530

    check number is odd or even

--- demo.js
+++ demo.js
@@ -2,3 +2,14 @@ var name = prompt("Enter your Name")
 var age = prompt("Enter your Age")
 AgeInDays = age * 365.25
 alert("Hai " + name + " You have lived in the world for " + AgeInDays + " days ")
+
+
+//Find the given number is odd or even
+var num = prompt("Enter any number")
+if(num%2==0){
+    console.log("Even Number")
+}
+else{
+     console.log("Odd Number")
+}
+
