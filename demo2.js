8540db3755ed3c0a5c08ee5b8d7cbf53e2613162
Esakkiammal <103570885+Esai27@users.noreply.github.com>
2023-09-04 11:17:00 +0530

    add while condition for print the divisor

--- demo2.js
+++ demo2.js
@@ -1,17 +1,26 @@
-var word="Hello"
-var i=1
-while(i<=10){
-    console.log(word )
-    i++
-}
+// var word="Hello"
+// var i=1
+// while(i<=10){
+//     console.log(word )
+//     i++
+// }
 
-//printing each letter in a word
-var name = prompt("Enter Your Name ")
-console.log(name.length)
-i =0
-while(i<=name.length){
-    console.log(name[i])
-    i++
-}
+// //printing each letter in a word
+// var name = prompt("Enter Your Name ")
+// console.log(name.length)
+// i =0
+// while(i<=name.length){
+//     console.log(name[i])
+//     i++
+// }
 
 //finding the divisor
+var num = prompt("Enter any number")
+divisor = 2
+while(divisor<num)
+{
+if (num%divisor==0){
+    console.log(divisor)
+}
+divisor++
+}
