a8f7243ec54be1e15255645ce45972c128cdaadd
Esakkiammal <103570885+Esai27@users.noreply.github.com>
2023-09-04 09:53:44 +0530

    printing the character and length ofchar in input

---
+++ demo2.js
@@ -0,0 +1,15 @@
+var word="Hello"
+var i=1
+while(i<=10){
+    console.log(word )
+    i++
+}
+
+//printing each letter in a word
+var name = prompt("Enter Your Name ")
+console.log(name.length)
+i =0
+while(i<=name.length){
+    console.log(name[i])
+    i++
+}
